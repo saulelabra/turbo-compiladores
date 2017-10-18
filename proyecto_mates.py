c33de750d05702edde084d9bfbef7450e2473c9d
axgtz <axguti@gmail.com>
2017-10-18 13:01:10 -0500

    Added comments

--- proyecto_mates.py
+++ proyecto_mates.py
@@ -1,5 +1,7 @@
+#Enrique Lira & Alejandro Gutierrez - Proyecto Matematicas Computacionales
+#-------Clase DFA---------
 class DFA:
-	def __init__(self, states, alphabet, transition_function, start_state, accept_states):
+	def __init__(self, states, alphabet, transition_function, start_state, accept_states):#Constructor, functions and variables declared
 		self.states = states;
 		self.alphabet = alphabet;
 		self.transition_function = transition_function;
@@ -8,7 +10,7 @@ class DFA:
 		self.current_state = start_state;
 		return;
 	
-	def transition_to_state_with_input(self, input_value):
+	def transition_to_state_with_input(self, input_value):#
 		if ((self.current_state, input_value) not in self.transition_function.keys()):
 			self.current_state = None;
 			return;
@@ -16,7 +18,7 @@ class DFA:
 		print("Transicion q{}".format(self.current_state));
 		return;
 	
-	def in_accept_state(self):
+	def in_accept_state(self):#Checa si el estado actual esta en los estados de aceptacion
 		return self.current_state in accept_states;
 	
 	def go_to_initial_state(self):
@@ -31,12 +33,17 @@ class DFA:
 		return self.in_accept_state();
 	pass;
 
+#-------Clase DFA Ends---------
+
+#-----------Main--------------
 cadena = raw_input("Dime una cadena: ");
 i=0;
 myList=[];
 tf = dict();
 
+#Using open, the text file called "protecto.txt" is loadad onto the variable 'f' using the parameter 'r' for reading only mode
 f = open('proyecto.txt', 'r')
+
 
 for line in f.readlines():
     if i >= 4:
@@ -48,16 +55,19 @@ for line in f.readlines():
         myList.append(line)
     i=i+1;
 
-start_state=int(myList[2])
-states = {myList[0]};
-alphabet = {myList[1]};
-accept_states = {int(myList[3])}
-f.close()
+start_state=int(myList[2])		#Add the start
+states = {myList[0]};			#
+alphabet = {myList[1]};			#
+accept_states = {int(myList[3])}#
+f.close() #Reading the file ends
+
+#Printing Results
 print("\nEstados de transicion:");
-d = DFA(states, alphabet, tf, start_state, accept_states);
+d = DFA(states, alphabet, tf, start_state, accept_states); #Transition states printing
 
 inp_program = list(cadena);
- 
+
+ #Runs the function of input list, if string ended on a valid state it prints "aceptada" if it ends on another state that is not accepted then it prints "no aceptada"
 if (d.run_with_input_list(inp_program)==True):
     print("\nACEPTADA");
 else: 
