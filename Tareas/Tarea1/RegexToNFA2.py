f6544d3e2814cf5a8699c5f24142176a3a6c3c6a
axgtz <axguti@gmail.com>
2020-01-23 09:12:34 -0600

    python obj

--- Tareas/Tarea1/RegexToNFA2.py
+++ Tareas/Tarea1/RegexToNFA2.py
@@ -19,8 +19,11 @@ class Stack:
         return 1
 
 class State:
-    def __init__(self, transitions):
-        
+    def __init__(self, isEnd):
+        # self.isStart = isStart
+        self.isEnd = isEnd
+        self.epsilonTransitions = []
+        self.transitions = {}
 
 class NFA:
     def __init__(self, initial_state, final_state, states, alphabet, transitions_states_table):
@@ -31,79 +34,124 @@ class NFA:
         self.transitions_states = transitions_states_table
         return
 
+
+
+    # Inserts a . when a concatenation should happen
+def insertConcatenationOperator(regex):
+    return None
+
     # Turns the infix expression to a postfix as it is easier to evaluate by order of precedence
     # As defined per Thompson paper
-    def inFixToPostFix
+def inFixToPostFix(regesInfix):
+    return None
 
 
-    def createEpsilonTrans(self, from, to):
-        # Create Start and End
-        # Receive number of the from state and to where is it going
-        #from.epsilonTransition.push(to)
-        return start, end
-    
-    def createSymbolTrans(self, symbol):
-        # Create Start and End state
-        # Receive number of the from state and to where is it going
-        # from transition to with 
-        return start, end
+def createEpsilonTrans():
+    # Create Start and End
+    start = State(False)
+    end = State(True)
+    # Receive number of the from state and to where is it going
+    start.epsilonTransitions.append(end) # add epsilon transition
+    #from.epsilonTransition.push(to)
+    return {start, end}
+
+def createSymbolTrans(symbol):
+    # Create Start and End state
+    start = State(False)
+    end = State(True)
+    # Receive number of the from state and to where is it going
+    start.transitions[symbol] = end
+    # from transition to with 
+    return {start, end}
 
 
-    # Concat
-    def createConcatTrans(): # NFA A and NFA B
-        # Create epsilon between NFA A end and NFA B start
-        # Disable end from NFA A end
-        return  start, end # NFA A start and NFA B end
+# Concat
+def createConcatTrans(first, second): # NFA A and NFA B
+    # Create epsilon between NFA A end and NFA B start
+    first.end.epsilonTransitions.append(second.start)
+    first.end.isEnd = False
+    # Disable end from NFA A end
+    return  {first.start, second.end} # NFA A start and NFA B end
 
-    # Union                     |
-    def createUnionTrans():# NFA A and NFA B
-        # Create start state
-        # Epsilon trans to NFA from start and eps trans to NFA B from start
+# Union                     |
+def createUnionTrans(first, second):# NFA A and NFA B
+    # Create start state
+    start = State(False)
+    # Epsilon trans to NFA from start and eps trans to NFA B from start
+    start.epsilonTransitions.append(first.start)
+    start.epsilonTransitions.append(second.start)
+    # Create end  state
+    end = State(True)
+    # Epsilon trans from NFA A to end state
+    first.end.epsilonTransitions.append(end)
+    # Epsilon trans from NFA B to end state
+    second.end.epsilonTransitions.append(end)
+    # Disable NFA A and NFA B ends
+    first.end.isEnd = False
+    second.end.isEnd = False
 
-        # Create end  state
-        # Epsilon trans from NFA A to end state
-        # Epsilon trans from NFA B to end state
-        # Disable NFA A and NFA B ends
-        return start, end
+    return {start, end}
 
-    # Kleene Star, Closure      *
-    def createClosureTrans(): # NFA
-        # Create start and end state
-        # Add epsilon trans start to end
-        # Add epsilon trans start to NFA start
+# Kleene Star, Closure      *
+def createClosureTrans(nfa): # NFA
+    # Create start and end state
+    start = State(False)
+    end = State(True)
+    # Add epsilon trans start to end
+    start.epsilonTransitions.append(end)
+    # Add epsilon trans start to NFA start
+    start.epsilonTransitions.append(nfa.start)
+    # Add epsilon trans NFA end to end
+    nfa.end.epsilonTransitions.append(end)
+    # Add epsilon trans NFA end to NFA start
+    nfa.end.epsilonTransitions.append(nfa.start)
+    # Disable nfa end 
+    nfa.end.isEnd = False
 
-        # Add epsilon trans NFA end to end
-        # Add epsilon trans NFA end to NFA start
+    return {start, end}
 
-        # Disable nfa end 
+# Kleene Plus               +
+# Same as Kleense Star but without epsilon skipping everything
+def createPlusTrans(nfa):
+    # Create start and end state
+    start = State(False)
+    end = State(True)
+    # Add epsilon trans start to NFA start
+    start.epsilonTransitions.append(nfa.start)
+    # Add epsilon trans NFA end to end
+    nfa.end.epsilonTransitions.append(end)
+    # Add epsilon trans NFA end to NFA start
+    nfa.end.epsilonTransitions.append(nfa.start)
+    # Disable nfa end 
+    nfa.end.isEnd = False
 
-        return start, end
+    return {start, end}
 
-    # Kleene Plus               +
-    # Same as Kleense Star but without epsilon skipping everything
-    def createPlusTrans():
-        # Create start and end state
-        # Add epsilon trans start to NFA start
+# Regex to NFA creator
+def createNFA(postFixRegex):
+    if postFixRegex == '' or postFixRegex == ' ': # empty regex
+        return createEpsilonTrans()# epsilon transition
 
-        # Add epsilon trans NFA end to end
-        # Add epsilon trans NFA end to NFA start
+    # Empty Stack
+    stack = Stack([])
 
-        # Disable nfa end 
+    for token in postFixRegex:
+        if token == "*":        # Star, closure
+            stack.push(createClosureTrans(stack.pop()))
+        elif token == "|":      # Union
+            first = stack.pop()
+            second = stack.pop()
+            stack.push(createUnionTrans(first, second))
+        elif token == ".":      # Concat
+            first = stack.pop()
+            second = stack.pop()
+            stack.push(createConcatTrans(first, second))
+        else:                    # Character 
+            stack.push(createSymbolTrans(token))
+            print("Token", token, " ", stack.top() ,"\n")
 
-        return start, end
+    print(stack.top())
+    return stack.pop() # Only one NFA should be there 
 
-    # Regex to NFA creator
-    def createNFA(self, postFixRegex):
-        if postFixRegex == '' OR postFixRegex == ' ': # empty regex
-            return # epsilon transition
 
-        # Empty Stack
-        stack = []
-
-        for token in postFixRegex:
-            if token == "*":        # Star
-            
-            elif token == "|":      # Union
-            
-            elif token == ".":      # Concat
-
+createNFA("ab.")
